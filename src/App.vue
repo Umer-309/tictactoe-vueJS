be322892b8825cc5af7197094e36771f907abf21
Muhammad Umer Nisar <unisar309@gmail.com>
2024-10-23 17:56:51 +0500

    Fixes production Build

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import CompositionTictac from './components/compositionTictac.vue'
+import CompositionTictac from './components/CompositionTictac.vue'
 
 export default defineComponent({
   components: {
